46273c5bdf8d5b70abbcafa834b9f437c94be0ea
JangHyeok-K <48301140+d0orHyeok@users.noreply.github.com>
2019-03-07 10:38:11 +0900

    C와 유사한 Java 문법 맛보기
    For, While은 안했음

---
+++ 0307/src/review_C/DoubleTest.java
@@ -0,0 +1,14 @@
+package review_C;
+
+public class DoubleTest {
+	public static void main(String[] args) {
+		double num1, num2;
+		double result;
+		
+		num1 = 1.0001;
+		num2 = 2.0001;
+		result = num1 + num2;
+		
+		System.out.println(result);
+	}
+}
---
+++ 0307/src/review_C/IfElse.java
@@ -0,0 +1,13 @@
+package review_C;
+
+public class IfElse {
+	public static void main(String[] args) {
+		int x = 5;
+		
+		if(x >= 5) {
+			System.out.println("x >= 5");
+		}else {
+			System.out.println("x < 5");
+		}
+	}
+}
